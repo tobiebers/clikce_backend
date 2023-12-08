b67578917f799203df3c6ffce1549ced76d16095
supertobi2001@gmail.com <16t1017E>
2023-12-08 11:58:18 +0100

    UserDashboard.py Fetch Chartresource gekürtz

--- resources/UserDashboard.py
+++ resources/UserDashboard.py
@@ -7,33 +7,19 @@ db = JsonDatabase('database_clone/UserQuestionsDataBase.json')
 
 class FetchChart(Resource):
     def get(self):
-        # Annahme: Du speicherst den Text für Abschnitt 1 im Backend
+        # Annahme: Du hast bereits das Dictionary erstellt
+        datensatz = {
+            'visitors1': 40,
+            'visitors2': 80,
+            'visitors3': 20,
+            'visitors4': 40,
+            'visitors5': 60,
+            'visitors6': 0,
+            'visitors7': 100,
+            'visitors8': 20,
+            'visitors9': 60,
+            'visitors10': 40
+        }
 
-
-        visitors1_text = 40
-        visitors2_text = 80
-        visitors3_text = 20
-        visitors4_text = 40
-        visitors5_text = 60
-        visitors6_text = 0
-        visitors7_text = 100
-        visitors8_text = 20
-        visitors9_text = 60
-        visitors10_text = 40
-
-
-
-
-        return jsonify({
-            'visitors1': visitors1_text,
-            'visitors2': visitors2_text,
-            'visitors3': visitors3_text,
-            'visitors4': visitors4_text,
-            'visitors5': visitors5_text,
-            'visitors6': visitors6_text,
-            'visitors7': visitors7_text,
-            'visitors8': visitors8_text,
-            'visitors9': visitors9_text,
-            'visitors10': visitors10_text,
-
-        })
+        # Verwende jsonify, um das Dictionary als JSON zu formatieren und zurückzugeben
+        return jsonify(datensatz)
