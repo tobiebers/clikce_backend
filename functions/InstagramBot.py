bfa043bbf468554564bc64928526466c139fa93c
super <supertobi2001@gmail.com>
2024-01-22 19:23:53 +0100

    accounts update

--- functions/InstagramBot.py
+++ functions/InstagramBot.py
@@ -10,6 +10,7 @@ class InstagramBot:
     def __init__(self, username, password):
         self.client = Client()
         self.client.login(username, password)
+        self.log_list = []
 
     def get_followers(self, target_username, follower_count):
         user_id = self.client.user_id_from_username(target_username)
@@ -34,6 +35,15 @@ class InstagramBot:
     def send_message_to_user(self, user_id, message):
         self.client.direct_send(message, [user_id])
 
+    def log_and_print(self, message):
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = {"timestamp": timestamp, "message": message}
+        self.log_list.append(log_entry)
+        print(message)
+        with open("log_file.json", "w") as file:
+            json.dump(self.log_list, file, indent=4)
+
+
 
 
 
@@ -42,48 +52,46 @@ def execute_bot_actions(username, password, duration, follower_count, target_use
     follower_count = int(follower_count)
     duration_interaction = duration * 24 * 60 * 60 / follower_count
 
-    # Erstellen einer Instanz von InstagramBot
     bot = InstagramBot(username, password)
-    print("erfolgreich eingeloggt")
+    bot.log_and_print("erfolgreich eingeloggt")
 
     follower_ids = bot.get_followers(target_username, follower_count)
-
     if not follower_ids:
-        print("Fehler beim Abrufen der Follower oder keine Follower gefunden.")
+        bot.log_and_print("Fehler beim Abrufen der Follower oder keine Follower gefunden.")
         return
 
     for user_id in follower_ids:
         try:
             bot.follow_user(user_id)
-            print("Nutzer", user_id, "gefolgt")
+            bot.log_and_print(f"Nutzer {user_id} gefolgt")
 
             last_post_id = bot.get_last_post_of_user(user_id)
             if last_post_id:
                 if like_posts:
                     try:
                         bot.like_latest_post(last_post_id)
-                        print("Beitrag geliked", last_post_id)
+                        bot.log_and_print(f"Beitrag geliked {last_post_id}")
                     except Exception as e:
-                        print(f"Fehler beim Liken des Beitrags: {e}")
+                        bot.log_and_print(f"Fehler beim Liken des Beitrags: {e}")
 
                 if comment_on_posts:
                     try:
                         bot.comment_post_by_id(last_post_id, comment_input)
-                        print("Post kommentiert mit der ID", last_post_id)
+                        bot.log_and_print(f"Post kommentiert mit der ID {last_post_id}")
                     except Exception as e:
-                        print(f"Fehler beim Kommentieren: {e}")
+                        bot.log_and_print(f"Fehler beim Kommentieren: {e}")
             else:
-                print(f"Keine Beiträge gefunden für Nutzer {user_id}")
+                bot.log_and_print(f"Keine Beiträge gefunden für Nutzer {user_id}")
 
         except Exception as e:
-            print(f"Fehler bei Interaktion mit Nutzer {user_id}: {e}")
+            bot.log_and_print(f"Fehler bei Interaktion mit Nutzer {user_id}: {e}")
 
         if send_message:
             try:
                 bot.send_message_to_user(user_id, message_input)
-                print("Nachricht gesendet an Nutzer", user_id)
+                bot.log_and_print(f"Nachricht gesendet an Nutzer {user_id}")
             except Exception as e:
-                print(f"Fehler beim Senden der Nachricht: {e}")
+                bot.log_and_print(f"Fehler beim Senden der Nachricht: {e}")
 
         time.sleep(duration_interaction)
 
--- resources/connect_accounts/account_functions.py
+++ resources/connect_accounts/account_functions.py
@@ -111,8 +111,6 @@ class ScheduleBulkPosts(Resource):
 
 
 
-from flask import request, jsonify
-from flask_restful import Resource
 
 class CreateBot(Resource):
     def post(self):
